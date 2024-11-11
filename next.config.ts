7e29ec4d852a4e386bcd86d18b02c7418378c37a
spkumar9878 <surya.12005525@lpu.in>
2024-11-11 20:21:53 +0530

    m

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
